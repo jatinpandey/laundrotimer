d6a4ba5ae3141cf7a4c4bd954faa4f1e67a34af2
jatinp <jatinp@unknown-10-73-204-x.yahoo.com>
2015-09-06 23:33:08 -0700

    Initial Commit

---
+++ Laundrotimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Laundrotimer
+//
+//  Created by Jatin Pandey on 9/6/15.
+//  Copyright © 2015 Jatin Pandey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
